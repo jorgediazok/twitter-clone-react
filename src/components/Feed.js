33897a5b9a1d96bd12a1ca462a9a958e8e51cafa
Jorge <jorgediazok@gmail.com>
2020-10-13 16:23:49 -0300

    created widget component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react'
 import TweetBox from "./TweetBox"
 import Post from "./Post"
--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import SearchIcon from "@material-ui/icons/Search"
+import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from "react-twitter-embed"
 import "../styles/Widgets.css"
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h1>Widgets</h1>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter"/>
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What is happening?</h2>
+        <TwitterTweetEmbed tweetId={"1277210300811534337"}/>
+        <TwitterTimelineEmbed sourceType="profile" screenName="porquetendencia" options={{height: 400}}/>
+        <TwitterShareButton url={"https://github.com/jorgediazok"} options={{text: "React is awesome", via: "roke_diaz"}}/>
+        
+      </div>
     </div>
   )
 }
